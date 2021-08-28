e84954da7f1b044f6f0c6c092823fe4f74411275
PoovizhiP <87531384+PoovizhiP@users.noreply.github.com>
2021-08-28 14:00:37 +0530

    java assignment commit

--- kcgjavaassignments/sizedatatyp.java
+++ kcgjavaassignments/sizedatatyp.java
@@ -2,12 +2,12 @@ public class SizeOfDataTypes {
 
     public static void main(String[] args) {
 
-        System.out.println("Byte      " + Byte.BYTES+" Bytes");
-        System.out.println("Character " + Character.BYTES+" Bytes");
-        System.out.println("Integer   " + Integer.BYTES+" Bytes");
-        System.out.println("Long      " + Long.BYTES+" Bytes");
-        System.out.println("Short     " + Short.BYTES+" Bytes");
-        System.out.println("Float     " + Float.BYTES+" Bytes");
-        System.out.println("Double    " + Double.BYTES+" Bytes");
+        System.out.println("Size of Byte      " + Byte.BYTES+" Bytes");
+        System.out.println("Size of Character " + Character.BYTES+" Bytes");
+        System.out.println("Size of Integer   " + Integer.BYTES+" Bytes");
+        System.out.println("Size of Long      " + Long.BYTES+" Bytes");
+        System.out.println("Size of Short     " + Short.BYTES+" Bytes");
+        System.out.println("Size of Float     " + Float.BYTES+" Bytes");
+        System.out.println("Size of Double    " + Double.BYTES+" Bytes");
     }
 }
