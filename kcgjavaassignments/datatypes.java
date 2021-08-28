5b08e45c3de40d0882ebcf52add6c8ab981a81c2
PoovizhiP <87531384+PoovizhiP@users.noreply.github.com>
2021-08-28 12:04:58 +0530

    First Assignment Commit

---
+++ kcgjavaassignments/datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String args[]) {
+      int a=200;
+      float b=89.8768f;
+      double c=6542;
+      char d='J';
+      String e="Java program for data types" ;
+
+      System.out.println("int = "+a);
+      System.out.println("Float = "+b);
+      System.out.println("Double = "+c);
+      System.out.println("Character = "+d);
+      System.out.println("String = "+e);
+    }
+}
---
+++ kcgjavaassignments/sizedatatyp.java
@@ -0,0 +1,13 @@
+public class SizeOfDataTypes {
+
+    public static void main(String[] args) {
+
+        System.out.println("Byte      " + Byte.BYTES+" Bytes");
+        System.out.println("Character " + Character.BYTES+" Bytes");
+        System.out.println("Integer   " + Integer.BYTES+" Bytes");
+        System.out.println("Long      " + Long.BYTES+" Bytes");
+        System.out.println("Short     " + Short.BYTES+" Bytes");
+        System.out.println("Float     " + Float.BYTES+" Bytes");
+        System.out.println("Double    " + Double.BYTES+" Bytes");
+    }
+}
